3b536b47363ec2a57bf5654b438fbc0665f2066f
KaynehacK <kaynehack18@gmail.com>
2025-04-18 21:16:08 +0500

    First commit

---
+++ 1/Task01.h
@@ -0,0 +1,8 @@
+//
+// Created by Роман on 17.04.2025.
+//
+
+#ifndef LAB1_TASK01_H
+#define LAB1_TASK01_H
+
+#endif //LAB1_TASK01_H
---
+++ 2/Task02.h
@@ -0,0 +1,8 @@
+//
+// Created by Роман on 18.04.2025.
+//
+
+#ifndef LAB1_TASK02_H
+#define LAB1_TASK02_H
+
+#endif //LAB1_TASK02_H
